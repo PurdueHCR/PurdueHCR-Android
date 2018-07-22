ebb4c89bd12434d4d10099101ceea99df9d237ae
Jason Li <jasonli468@gmail.com>
2018-07-22 17:23:16 -0500

    signup fully working

--- app/src/main/java/com/hcrpurdue/jason/hcrhousepoints/Authentication.java
+++ app/src/main/java/com/hcrpurdue/jason/hcrhousepoints/Authentication.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Pair;
 import android.util.Patterns;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -12,19 +13,30 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Authentication extends AppCompatActivity {
     private FirebaseAuth auth;
+    private Map<String, Pair<String, String>> floorCodes;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
         auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        getFloorCodes();
     }
 
     @Override
@@ -57,8 +69,6 @@ public class Authentication extends AppCompatActivity {
                             Toast.makeText(getApplicationContext(), "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
-
-                        // ...
                     }
                 });
     }
@@ -69,11 +79,10 @@ public class Authentication extends AppCompatActivity {
         EditText passwordConfirm = findViewById(R.id.confirmPasswordInput);
         EditText name = findViewById(R.id.nameInput);
         EditText floorCode = findViewById(R.id.floorCodeInput);
-        String emailText = email.getText().toString();
-        String passwordText = password.getText().toString();
-        String passwordConfirmText = passwordConfirm.getText().toString();
-        String nameText = name.getText().toString();
-        String floorCodeText = floorCode.getText().toString();
+        final String emailText = email.getText().toString();
+        final String passwordText = password.getText().toString();
+        final String nameText = name.getText().toString();
+        final String floorCodeText = floorCode.getText().toString();
 
 
         if (signInInvalid(email, password) || signUpInvalid(password, passwordConfirm, name, floorCode))
@@ -85,6 +94,24 @@ public class Authentication extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             FirebaseUser user = auth.getCurrentUser();
+
+                            // Generates all of the other data for the user in the DB
+                            Map<String, Object> userData = new HashMap<>();
+                            userData.put("Name", nameText);
+                            userData.put("FloorID", floorCodes.get(floorCodeText).first);
+                            userData.put("House", floorCodes.get(floorCodeText).second);
+                            userData.put("Permission Level", 0);
+                            userData.put("TotalPoint", 0);
+                            if(user != null)
+                                db.collection("Users").document(user.getUid()).set(userData)
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(getApplicationContext(), "User DB binds failed, please tell your RHP to tell Jason", Toast.LENGTH_LONG).show();
+                                            }
+                                        });
+                            else
+                                Toast.makeText(getApplicationContext(), "User was created but not loaded, please tell your RHP to tell Jason", Toast.LENGTH_SHORT).show();
                             // TODO: Show rest of stuff after signed in
                         } else {
                             Toast.makeText(getApplicationContext(), "Authentication failed, try again later before contacting your RHP",
@@ -141,8 +168,37 @@ public class Authentication extends AppCompatActivity {
             return true;
         }
 
-        //TODO: Check that FloorCode matches a valid code
+        // Checks that the floor code is valid
+        if(floorCodes.get(floorCodeText)  == null){
+            Toast.makeText(getApplicationContext(), "Floor code invalid", Toast.LENGTH_SHORT).show();
+            return true;
+        }
 
         return false;
+    }
+
+    // Fills floorCodes with key:value pairs in the form of {floorCode}:({floorName}:{houseName})
+    private void getFloorCodes(){
+        floorCodes = new HashMap<>();
+        db.collection("House").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document : task.getResult())
+                    {
+                        Map<String, Object> data = document.getData();
+                        for(Map.Entry<String, Object> entry : data.entrySet())
+                        {
+                            if(entry.getKey().contains("Code") && entry.getValue().getClass() == entry.getKey().getClass())
+                            {
+                                floorCodes.put((String)entry.getValue(), new Pair<>(entry.getKey().replace("Code", ""), document.getId()));
+                            }
+                        }
+                    }
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Error retrieving Floor codes from database, please try again later before contacting your RHP", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
