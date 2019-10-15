f37578903a92e302d27a70239334466763716dfe
Brian Johncox <brianjohncox232@gmail.com>
2019-10-14 20:39:19 -0400

    Base Code for Changing the Header

--- app/src/main/java/com/hcrpurdue/jason/hcrhousepoints/Fragments/ProfileFragment.java
+++ app/src/main/java/com/hcrpurdue/jason/hcrhousepoints/Fragments/ProfileFragment.java
@@ -1,54 +1,27 @@
 package com.hcrpurdue.jason.hcrhousepoints.Fragments;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.formatter.IValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.utils.ViewPortHandler;
-import com.hcrpurdue.jason.hcrhousepoints.Models.PointLog;
 import com.hcrpurdue.jason.hcrhousepoints.R;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-
-import com.hcrpurdue.jason.hcrhousepoints.Models.House;
-import com.hcrpurdue.jason.hcrhousepoints.Models.Reward;
 import com.hcrpurdue.jason.hcrhousepoints.Utils.CacheManager;
 import com.hcrpurdue.jason.hcrhousepoints.Utils.FirebaseListenerUtil;
 import com.hcrpurdue.jason.hcrhousepoints.Utils.UtilityInterfaces.ListenerCallbackInterface;
-import com.hcrpurdue.jason.hcrhousepoints.Utils.UtilityInterfaces.CacheManagementInterface;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment implements ListenerCallbackInterface {
     private Context context;
     private FirebaseListenerUtil flu;
+    private CacheManager cacheManager;
     private final String PERSONAL_LOGS_CALLBACK_KEY = "POINT_LOG_DETAILS";
 
     @Override
@@ -60,6 +33,7 @@ public class ProfileFragment extends Fragment implements ListenerCallbackInterfa
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cacheManager = CacheManager.getInstance(context);
     }
 
     @Override
@@ -72,7 +46,9 @@ public class ProfileFragment extends Fragment implements ListenerCallbackInterfa
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_new_profile, container, false);
-
+        Toolbar toolbar = view.findViewById(R.id.profile_toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle(cacheManager.getName());
         return view;
     }
 
@@ -80,6 +56,7 @@ public class ProfileFragment extends Fragment implements ListenerCallbackInterfa
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         AppCompatActivity activity = (AppCompatActivity) getActivity();
+        Objects.requireNonNull(activity.getSupportActionBar()).setTitle(cacheManager.getName());
     }
 
 
