2b986d7b028d1968f11bf42f4ed6f1749a76bcd1
Brian Johncox <brianjohncox232@gmail.com>
2019-11-11 21:55:34 -0500

    Basics for changing tool bar

--- app/src/main/java/com/hcrpurdue/jason/hcrhousepoints/Fragments/ProfileFragment.java
+++ app/src/main/java/com/hcrpurdue/jason/hcrhousepoints/Fragments/ProfileFragment.java
@@ -3,11 +3,17 @@ package com.hcrpurdue.jason.hcrhousepoints.Fragments;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.hcrpurdue.jason.hcrhousepoints.R;
@@ -46,9 +52,11 @@ public class ProfileFragment extends Fragment implements ListenerCallbackInterfa
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_new_profile, container, false);
-        Toolbar toolbar = view.findViewById(R.id.profile_toolbar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        toolbar.setTitle(cacheManager.getName());
+       // Toolbar toolbar = view.findViewById(R.id.profile_toolbar);
+//        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        //((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+//        actionBar.setTitle(cacheManager.getName());
+        setHasOptionsMenu(true);
         return view;
     }
 
@@ -56,8 +64,13 @@ public class ProfileFragment extends Fragment implements ListenerCallbackInterfa
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         AppCompatActivity activity = (AppCompatActivity) getActivity();
-        Objects.requireNonNull(activity.getSupportActionBar()).setTitle(cacheManager.getName());
+        Objects.requireNonNull(activity.getSupportActionBar()).setTitle("Profile: "+cacheManager.getName());
     }
 
-
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.profile_settings_menu, menu);
+        MenuItem settingsButton = menu.findItem(R.id.action_settings_button);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
